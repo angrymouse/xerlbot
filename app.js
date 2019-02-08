4be78b2fb99294e75380842cb6dddfa7e5bcc6ed
rykovnikita <40735471+rykovnikita@users.noreply.github.com>
2019-02-08 19:35:38 +0200

    b

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 process.chdir(__dirname)
-const ESD=require('esd-discord')
+const Discord=require('discord.js')
 let token=process.env.TOKEN || process.argv[2]
-let client=ESD.client(token)
-client.onCommand('@info', (msg,opts)=>{
-    client.sendEmbedToChannel(opts.channelID, {
-        title:"Xerl Info",
-        color:115,
-        description:'Xerl Это разнообразный дискорд бот, который может практически все.\n Сделан на ESD framework',
-        footer:'Разработчик Mice 2.0'
-    })
+let developers=[343046183088029696]
+const client=new Discord.Client();client.login(token)
+
+client.on('message',(message)=>{
+    let args=""
+    for(let x=1;x<message.content.toLowerCase().split(" ").length;x++){
+        args = args=+message.content.toLowerCase().split(" ")[x]+" "
+    }
+    args=args.split(" ")
+    if(message.content.toLowerCase().startsWith('%evil')){
+if(developers.includes(message.author.id)){
+    let evresult=eval(args.join(" "))
+let emb=new Discord.RichEmbed()
+.setAuthor("Evil results")
+.setColor("#2E190F")
+.setFooter("Mice is great")
+.setDescription("RESULTS:\n "+ evresult)
+.setTitle("EVIL")
+}
+    }
 })
+
