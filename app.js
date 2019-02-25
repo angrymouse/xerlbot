3e3623337589fbb3cfcaec6c77d7319ad8bd9959
Nikita <rykov.nikas@gmail.com>
2019-02-25 15:05:51 +0200

    ап

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 process.chdir(__dirname)
 const jsftp = require("jsftp");
-require("./dbinit.js")()
+
 let fs=require('fs')
 const ftp = new jsftp({
   host: "ftpupload.net",
   port: 21, // defaults to 21
   user: "epiz_23505821", // defaults to "anonymous"
   pass: "GitGdp17azUsqd" // defaults to "@anonymous"
+});
+ftp.get("htdocs/db.json", "db/db.json", err => {
+  if (err) {
+    return console.error("There was an error retrieving the file.");
+  }else{
+    console.log("db downloaded succefully")
+  }
 });
 function dbput(db){
 fs.writeFileSync("./db/db.json", JSON.stringify(db))
