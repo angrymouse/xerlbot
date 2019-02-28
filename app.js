7e186a6b8a117c24861dbb4f3968b644c55c1ffe
Nikita <rykov.nikas@gmail.com>
2019-02-28 17:01:45 +0200

    d

--- app.js
+++ app.js
@@ -305,7 +305,7 @@ if(message.content.toLowerCase()==`${pr}protection enable`||message.content.toLo
     message.reply("Вы должны иметь право администратора сервера для использования этой команды!")
   }
 }
-if(message.content.toLowerCase().startsWith(`${pr}say`){
+if(message.content.toLowerCase().startsWith(`${pr}say`)){
   message.delete(100)
   message.channel.send(args)
 })
